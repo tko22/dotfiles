7779c7f4f50a35e1230cf414de67083c07f42b38
Timothy Ko <timothy.l.ko@gmail.com>
2018-04-04 15:40:05 -0500

    added install script

--- install.sh
+++ install.sh
@@ -5,42 +5,20 @@ xcode-select --install
 
 # Install Homebrew.
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install brew basics (auto-updating).
-brew install terminal-notifier
-brew tap domt4/autoupdate
-brew autoupdate --start --upgrade --cleanup --enable-notifications
+brew update
 
 # Install brew essentials.
 brew install heroku
 brew install git
 brew install ack
 
-# Install Heroku plugins.
-heroku plugins:install heroku-sudo
-heroku plugins:install heroku-repo
-heroku plugins:install heroku-buildkits
-
 # Install download utilities.
 brew install wget
 brew install httpie
 
 # Install fancy shell stuff.
-brew install fish
-brew install grc
-brew install direnv
-brew install nnn
-brew install thefuck
-brew install autojump
-brew install googler
-brew install mas
 brew install htop
-brew install neofetch
-brew install mosh
-
-# Install bash utilities.
-brew install bats
-brew install shellcheck
+brew install tmate
 
 # Install Python utlitlies.
 brew install python3
@@ -48,8 +26,6 @@ brew install python@2
 brew install pypy3
 
 # Python utilities.
-pip3 install legit
-pip2 install em-keyboard
 
 # Pipenv!
 brew install pipenv
@@ -60,13 +36,8 @@ brew install gist
 
 # Install other languages.
 brew install node
+brew upgrade node
 brew install ruby
-
-# Install fun stuff.
-brew install fortune
-brew install cowsay
-brew install sl
-gem install lolcat
 
 # Install network utilities
 brew install sshuttle
